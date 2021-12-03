a1df5c752c274677a7a31a3e62c396f1ca924936
Michael Mitchell <iamovyerus@gmail.com>
2021-12-03 12:51:06 +1100

    feat: ping

---
+++ lib/salad/commands/ping.ex
@@ -0,0 +1,19 @@
+defmodule Salad.Command.Ping do
+  @moduledoc false
+  use Bitwise
+  use Salad.CommandSystem.Command
+
+  @impl true
+  def description, do: "Pong"
+
+  @impl true
+  def run(ctx) do
+    reply(ctx, %{
+      type: 4,
+      data: %{
+        content: "Pong!",
+        flags: 1 <<< 6
+      }
+    })
+  end
+end
