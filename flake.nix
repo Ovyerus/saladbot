3a42ba54b96689233ca8fd775546406014505f12
Ovyerus <ovy@ovyerus.com>
2024-01-06 15:53:24 +1100

    feat: build docker image through nix

--- flake.nix
+++ flake.nix
@@ -11,43 +11,54 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        elixir = pkgs.beam.packages.erlang_26.elixir_1_16;
-        erlang = pkgs.beam.packages.erlang_26.erlang;
-      in {
-        devShells.default = with pkgs;
-          mkShell {
-            nativeBuildInputs =
-              []
-              ++ (lib.optional pkgs.stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-                CoreFoundation
-              ]));
+        # Setup
+        pkgs = import nixpkgs {inherit system;};
+        beam = pkgs.beam.packages.erlang_26;
 
+        elixir = beam.elixir_1_16;
+        erlang = beam.erlang;
+
+        nativeBuildInputs = with pkgs;
+          []
+          ++ (lib.optional stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+            CoreFoundation
+          ]));
+
+        # Build release
+        pname = "salad";
+        version = "0.1.0";
+        src = ./.;
+
+        mixFodDeps = beam.fetchMixDeps {
+          inherit src version elixir erlang;
+          pname = "mix-deps-${pname}";
+          hash = "sha256-DKnR2V5L76waN+EUDGvIOsgTechr67KAtrWt+CDRpxc=";
+        };
+
+        release = beam.mixRelease {
+          inherit pname version src elixir erlang mixFodDeps nativeBuildInputs;
+
+          # When running multi-node deploys, set `RELEASE_COOKIE` to override this.
+          removeCookie = false;
+        };
+
+        docker = pkgs.dockerTools.buildImage {
+          name = pname;
+          tag = "latest";
+          copyToRoot = [release];
+          config.Cmd = ["${release}/bin/salad" "start"];
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            inherit nativeBuildInputs;
             buildInputs = [elixir erlang];
           };
 
-        packages.default = let
-          beam = pkgs.beamPackages;
-
-          pname = "salad";
-          version = "0.1.0";
-          src = ./.;
-
-          mixFodDeps = beam.fetchMixDeps {
-            inherit src version elixir erlang;
-            pname = "mix-deps-${pname}";
-            hash = "sha256-DKnR2V5L76waN+EUDGvIOsgTechr67KAtrWt+CDRpxc=";
+          packages = {
+            inherit docker release;
+            default = release;
           };
-          # Set RELEASE_DISTRIBUTION=none by default?
-        in
-          beam.mixRelease {
-            inherit pname version src elixir erlang mixFodDeps;
-
-            # When running multi-node deploys, set `RELEASE_COOKIE` to override this.
-            removeCookie = false;
-          };
-      }
+        }
     );
 }
