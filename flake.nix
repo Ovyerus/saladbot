78bde47db971bfd33a7fcd89674cf0b900fd3ae4
Ovyerus <ovy@ovyerus.com>
2024-01-06 15:08:04 +1100

    feat: add package output in flake

--- flake.nix
+++ flake.nix
@@ -14,6 +14,8 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        elixir = pkgs.beam.packages.erlang_26.elixir_1_16;
+        erlang = pkgs.beam.packages.erlang_26.erlang;
       in {
         devShells.default = with pkgs;
           mkShell {
@@ -23,10 +25,28 @@
                 CoreFoundation
               ]));
 
-            buildInputs = [
-              beam.packages.erlang_26.elixir_1_16
-              beam.packages.erlang_26.erlang
-            ];
+            buildInputs = [elixir erlang];
+          };
+
+        packages.default = let
+          beam = pkgs.beamPackages;
+
+          pname = "salad";
+          version = "0.1.0";
+          src = ./.;
+
+          mixFodDeps = beam.fetchMixDeps {
+            inherit src version elixir erlang;
+            pname = "mix-deps-${pname}";
+            hash = "sha256-DKnR2V5L76waN+EUDGvIOsgTechr67KAtrWt+CDRpxc=";
+          };
+          # Set RELEASE_DISTRIBUTION=none by default?
+        in
+          beam.mixRelease {
+            inherit pname version src elixir erlang mixFodDeps;
+
+            # When running multi-node deploys, set `RELEASE_COOKIE` to override this.
+            removeCookie = false;
           };
       }
     );
